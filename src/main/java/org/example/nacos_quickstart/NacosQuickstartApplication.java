74c38d77ea9b9044b48e5d077268a566fd630006
googlg <911kissusa@gmail.com>
2025-06-17 07:23:03 +0800

    测试

---
+++ src/main/java/org/example/nacos_quickstart/NacosQuickstartApplication.java
@@ -0,0 +1,13 @@
+package org.example.nacos_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosQuickstartApplication.class, args);
+    }
+
+}
