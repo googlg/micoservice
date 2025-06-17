2b15747fcabde8c7e395ceba3e6de192ff29270b
googlg <911kissusa@gmail.com>
2025-06-17 13:38:10 +0800

    测试

--- src/main/java/org/example/nacos_quickstart/NacosQuickstartApplication.java
+++ src/main/java/org/example/nacos_quickstart/NacosQuickstartApplication.java
@@ -2,8 +2,10 @@ package org.example.nacos_quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NacosQuickstartApplication {
 
     public static void main(String[] args) {
